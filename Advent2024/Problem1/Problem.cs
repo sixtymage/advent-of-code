60d12ae270c25cf6f61956bcefc5c4c8d505ce54
Jon Hutchings <sixty.mage@outlook.com>
2024-12-06 18:06:22 +0200

    Add 2024 problem 1 part 2 solution

--- Advent2024/Problem1/Problem.cs
+++ Advent2024/Problem1/Problem.cs
@@ -1,5 +1,3 @@
-using System.Collections.Immutable;
-
 namespace Advent2024.Problem1;
 
 public class Problem(string filename = @"data\problem1-input.txt") : IProblem
@@ -8,6 +6,58 @@ public class Problem(string filename = @"data\problem1-input.txt") : IProblem
   {
     var lines = await File.ReadAllLinesAsync(filename);
 
+    var (first, second) = ExtractLists(lines);
+
+    SortLists(first, second);
+    ValidateLengths(first, second);
+
+    var distanceScore = CalculateDistanceScore(first, second);
+    var similarityScore = CalculateSimilarityScore(first, second);
+
+    Console.WriteLine($"Total distance is: {distanceScore}");
+    Console.WriteLine($"Similarity score is: {similarityScore}");
+  }
+
+  private object CalculateSimilarityScore(int[] first, int[] second)
+  {
+    var total = 0L;
+    for (var i = 0; i < first.Length; i++)
+    {
+      var count = second.Count(x => x == first[i]);
+      total += first[i] * count;
+    }
+
+    return total;
+  }
+
+  private static long CalculateDistanceScore(int[] first, int[] second)
+  {
+    var total = 0L;
+    for (var i=0; i<first.Length; i++)
+    {
+      var delta = Math.Abs(first[i] - second[i]);
+      total += delta;
+    }
+
+    return total;
+  }
+
+  private static void ValidateLengths(int[] first, int[] second)
+  {
+    if (first.Length != second.Length)
+    {
+      throw new InvalidDataException("Input arrays do not have the same length");
+    }
+  }
+
+  private static void SortLists(int[] first, int[] second)
+  {
+    Array.Sort(first);
+    Array.Sort(second);
+  }
+
+  private static (int[], int[]) ExtractLists(string[] lines)
+  {
     var numbers = lines
       .Select(x =>
       {
@@ -18,22 +68,6 @@ public class Problem(string filename = @"data\problem1-input.txt") : IProblem
 
     var first = numbers.Select(x => x.Item1).ToArray();
     var second = numbers.Select(x => x.Item2).ToArray();
-
-    if (first.Length != second.Length)
-    {
-      throw new InvalidDataException("Input arrays do not have the same length");
-    }
-   
-    Array.Sort(first);
-    Array.Sort(second);
-
-    var total = 0L;
-    for (var i=0; i<first.Length; i++)
-    {
-      var delta = Math.Abs(first[i] - second[i]);
-      total += delta;
-    }
-
-    Console.WriteLine($"Total distance is: {total}");
+    return (first, second);
   }
 }
--- Advent2024/Program.cs
+++ Advent2024/Program.cs
@@ -1,10 +1,47 @@
-﻿try
+﻿
+namespace Advent2024;
+
+internal static class Program
 {
-  var p = new Advent2024.Problem1.Problem();
-  await p.SolveAsync();
-}
-catch (Exception e)
-{
-  Console.ForegroundColor = ConsoleColor.Red;
-  Console.WriteLine($"Error: {e}");
+  public static async Task Main(string[] args)
+  {
+    try
+    {
+      var problemId = GetProblemId(args);
+      var problem = CreateProblem(problemId);
+      await problem.SolveAsync();
+    }
+    catch (Exception e)
+    {
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine($"Error: {e}");
+    }
+  }
+
+  private static int GetProblemId(string[] args)
+  {
+    return args.Length switch
+    {
+      > 0 when int.TryParse(args[0], out var problemId) => problemId,
+      _ => 0
+    };
+  }
+
+  private static IProblem CreateProblem(object problemId)
+  {
+    return problemId switch
+    {
+      1 => new Problem1.Problem(),
+      _ => new NullProblem()
+    };
+  }
+
+  private class NullProblem : IProblem
+  {
+    public Task SolveAsync()
+    {
+      Console.WriteLine("Usage: Advent2024.exe [problemId]");
+      return Task.CompletedTask;
+    }
+  }
 }

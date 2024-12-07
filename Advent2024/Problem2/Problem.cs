6654240811b8635893dc75e36a34f493accc93fb
Jon Hutchings <sixty.mage@outlook.com>
2024-12-07 11:14:20 +0200

    Add 2024 problem 2 part 2 solution

--- Advent2024/Problem2/Problem.cs
+++ Advent2024/Problem2/Problem.cs
@@ -8,54 +8,116 @@ public class Problem(string filename = @"data\problem2-input.txt") : IProblem
   {
     var lines = await File.ReadAllLinesAsync(filename);
 
-    var reports = ExtractReports(lines);
-    var safeReportCount = AnalyseReports(reports.AsReadOnly());
+    var reports = ExtractReports(lines).AsReadOnly();
 
-    Console.WriteLine($"The count of safe reports is {safeReportCount}");
+    var result1 = AnalyseReports(reports, 0);
+    var result2 = AnalyseReports(reports, 1);
+
+    ShowResult(result1);
+    ShowResult(result2);
   }
 
-  private static int AnalyseReports(ReadOnlyCollection<Report> reports)
+  private static void ShowResult(Result result)
+  {
+    Console.WriteLine($"With tolerance {result.Tolerance} the number of safe reports is {result.NumSafeReports}");
+  }
+
+  private static Result AnalyseReports(ReadOnlyCollection<Report> reports, int tolerance)
   {
     var count = 0;
     foreach (var report in reports)
     {
-      if (AreLevelsSafe(report.Levels))
+      if (AreLevelsSafe(report.Levels, tolerance))
       {
         count++;
       }
     }
-    return count;
+
+    return new Result
+    {
+      Tolerance = tolerance,
+      NumSafeReports = count
+    };
+  }
+
+  private static bool AreLevelsSafe(int[] levels, int tolerance)
+  {
+    if (AreLevelsSafe(levels))
+    {
+      return true;
+    }
+
+    // if we cannot tolerate any more reading removals we are done
+    if (tolerance == 0 || levels.Length == 2)
+    {
+      return false;
+    }
+
+    // otherwise lower the tolerance...
+    tolerance--;
+
+    // and try all possible ways of removing a reading
+    for (var i = 0; i < levels.Length; i++)
+    {
+      var alternateLevels = RemoveReading(levels, i);
+      if (AreLevelsSafe(alternateLevels, tolerance))
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private static int[] RemoveReading(int[] levels, int i)
+  {
+    if (i < 0 || i >= levels.Length)
+    {
+      throw new ArgumentOutOfRangeException(nameof(i));
+    }
+
+    // use the slicing syntax to join the respective parts, skipping the specified index
+    return levels[..i].Concat(levels[(i + 1)..]).ToArray();
   }
 
   private static bool AreLevelsSafe(int[] levels)
   {
-    // check difference between adjacent entries is at least 1 and no more than 3if (levels.Length == 0)
-    // all pairs are strictly increasing or strictly decreasing
-    long? direction = null;
+    // track the direction the levels are trending
+    var direction = 0;
+
+    // check pairs of readings until we spot unsafe readings
     for (var  i = 1; i < levels.Length; i++)
     {
-      long difference = levels[i] - levels[i - 1];
-      if (Math.Abs(difference) is < 1 or > 3)
+      if (!AreLevelReadingsSafe(levels[i - 1], levels[i], ref direction))
       {
         return false;
       }
-
-      // have we set the direction yet?
-      if (direction is not null)
-      {
-        // the direction and this difference must have the same sign
-        if (difference * direction < 0)
-        {
-          return false;
-        }
-
-        continue;
-      }
-
-      // if we have a definite direction, set it
-      direction = difference != 0 ? difference : null;
     }
 
+    return true;
+  }
+
+  private static bool AreLevelReadingsSafe(int leftReading, int rightReading, ref int direction)
+  {
+    // figure out the difference between the adjacent level readings, if any
+    long difference = rightReading - leftReading;
+
+    // now check if the difference is too much
+    if (Math.Abs(difference) is < 1 or > 3)
+    {
+      return false;
+    }
+
+    // now check if the level violates the prevailing direction if one exists
+    if (direction != 0 && difference * direction < 0)
+    {
+      return false;
+    }
+
+    // update the direction if it has not yet been set
+    direction = difference > 0 ? 1 : -1;
+
+    // the level is safe
     return true;
   }
 
---
+++ Advent2024/Problem2/Result.cs
@@ -0,0 +1,8 @@
+namespace Advent2024.Problem2;
+
+public record Result
+{
+  public int Tolerance { get; init; }
+
+  public int NumSafeReports { get; init; }
+}

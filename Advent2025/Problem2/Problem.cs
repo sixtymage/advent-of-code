4916fce7d2caa2b98931e276569176240a6cbd0e
Jon Hutchings <sixty.mage@outlook.com>
2025-12-02 23:26:53 +0200

    Tidy up

--- Advent2025/Problem2/Problem.cs
+++ Advent2025/Problem2/Problem.cs
@@ -8,28 +8,28 @@ public class Problem(string filename = @"data\problem2-input.txt") : IProblem
 
     var ranges = LoadRanges(lines[0]);
 
-    var sum = ExamineRanges(ranges);
+    var sum = ExamineRanges(ranges, true);
 
     Console.WriteLine($"Answer is: {sum}");
   }
 
-  private long ExamineRanges(List<Range> ranges)
+  private static long ExamineRanges(List<Range> ranges, bool allowMultiple)
   {
     long sum = 0;
     foreach (var range in ranges)
     {
-      sum += ExamineRange(range);
+      sum += ExamineRange(range, allowMultiple);
     }
 
     return sum;
   }
 
-  private long ExamineRange(Range range)
+  private static long ExamineRange(Range range, bool allowMultiple)
   {
     long sum = 0;
     for (long i = range.Start; i <= range.End; i++)
     {
-      if (IsRepeatedSequence(i))
+      if (IsRepeatedSequence(i, allowMultiple))
       {
         sum += i;
       }
@@ -37,7 +37,48 @@ public class Problem(string filename = @"data\problem2-input.txt") : IProblem
     return sum;
   }
 
-  private bool IsRepeatedSequence(long number)
+  private static bool IsRepeatedSequence(long number, bool allowMultiple)
+  {
+    if (allowMultiple)
+    {
+      return IsRepeatedSequenceAllowMultiple(number);
+    }
+    return IsDoubleSequence(number);
+  }
+
+  private static bool IsRepeatedSequenceAllowMultiple(long number)
+  {
+    var numberAsString = number.ToString();
+
+    // if only one digit, there can't be any repeats
+    if (numberAsString.Length <= 1)
+    {
+      return false;
+    }
+
+    // otherwise examine all sequence lengths that cleanly divide the length where all sequences are the same
+    for (var sequenceLength = 1; sequenceLength <= numberAsString.Length / 2; sequenceLength++)
+    {
+      if (numberAsString.Length % sequenceLength == 0)
+      {
+        var numSegments = numberAsString.Length / sequenceLength;
+        var segments = new List<string>();
+        for (int i=0; i<numberAsString.Length; i += sequenceLength)
+        {
+          var segment = numberAsString.Substring(i, sequenceLength);
+          segments.Add(segment);
+        }
+
+        if (segments.All(s => s == segments[0]))
+        {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  private static bool IsDoubleSequence(long number)
   {
     var numberAsString = number.ToString();
 
